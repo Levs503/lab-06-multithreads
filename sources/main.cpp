c773da767eac5fea1749c72dff0ea5e6acf0d856
Lev <semkov01@mail.ru>
2020-12-16 23:08:52 +0500

    begin

---
+++ sources/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Лев on 16.12.2020.
+//
+#include <picosha2.h>
+#include<iostream>
+#include<thread>
+
+int main(){
+  std::cout<<"sadfg";
+  std::cout<<std::thread::hardware_concurrency();
+}
